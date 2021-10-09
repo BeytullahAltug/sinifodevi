11929d93c69d7eccfe802e22a4de33e3b17e58d4
beytu <beytu@MARSL1>
2021-10-09 21:33:47 +0300

    Proje dosyası ekle.

---
+++ sınıf ödevi/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace sınıf_ödevi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_göster = new System.Windows.Forms.Button();
+            this.Sınıfllar = new System.Windows.Forms.ListBox();
+            this.txt_katnumarası = new System.Windows.Forms.TextBox();
+            this.lbl_katnumarası = new System.Windows.Forms.Label();
+            this.lbl_bilgi = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btn_temizle = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.groupBox1.Controls.Add(this.btn_temizle);
+            this.groupBox1.Controls.Add(this.btn_göster);
+            this.groupBox1.Controls.Add(this.Sınıfllar);
+            this.groupBox1.Controls.Add(this.txt_katnumarası);
+            this.groupBox1.Controls.Add(this.lbl_katnumarası);
+            this.groupBox1.Controls.Add(this.lbl_bilgi);
+            this.groupBox1.Location = new System.Drawing.Point(61, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(301, 248);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btn_göster
+            // 
+            this.btn_göster.Location = new System.Drawing.Point(56, 118);
+            this.btn_göster.Name = "btn_göster";
+            this.btn_göster.Size = new System.Drawing.Size(75, 23);
+            this.btn_göster.TabIndex = 1;
+            this.btn_göster.Text = "Göster";
+            this.btn_göster.UseVisualStyleBackColor = true;
+            this.btn_göster.Click += new System.EventHandler(this.btn_göster_Click);
+            // 
+            // Sınıfllar
+            // 
+            this.Sınıfllar.FormattingEnabled = true;
+            this.Sınıfllar.Location = new System.Drawing.Point(6, 147);
+            this.Sınıfllar.Name = "Sınıfllar";
+            this.Sınıfllar.Size = new System.Drawing.Size(289, 95);
+            this.Sınıfllar.TabIndex = 3;
+            // 
+            // txt_katnumarası
+            // 
+            this.txt_katnumarası.Location = new System.Drawing.Point(135, 72);
+            this.txt_katnumarası.Name = "txt_katnumarası";
+            this.txt_katnumarası.Size = new System.Drawing.Size(100, 20);
+            this.txt_katnumarası.TabIndex = 2;
+            // 
+            // lbl_katnumarası
+            // 
+            this.lbl_katnumarası.AutoSize = true;
+            this.lbl_katnumarası.Location = new System.Drawing.Point(53, 79);
+            this.lbl_katnumarası.Name = "lbl_katnumarası";
+            this.lbl_katnumarası.Size = new System.Drawing.Size(76, 13);
+            this.lbl_katnumarası.TabIndex = 1;
+            this.lbl_katnumarası.Text = "Kat Numarası :";
+            // 
+            // lbl_bilgi
+            // 
+            this.lbl_bilgi.AutoSize = true;
+            this.lbl_bilgi.Location = new System.Drawing.Point(87, 16);
+            this.lbl_bilgi.Name = "lbl_bilgi";
+            this.lbl_bilgi.Size = new System.Drawing.Size(116, 13);
+            this.lbl_bilgi.TabIndex = 0;
+            this.lbl_bilgi.Text = "Okul Kat Yerleşim Planı";
+            // 
+            // btn_temizle
+            // 
+            this.btn_temizle.Location = new System.Drawing.Point(160, 118);
+            this.btn_temizle.Name = "btn_temizle";
+            this.btn_temizle.Size = new System.Drawing.Size(75, 23);
+            this.btn_temizle.TabIndex = 4;
+            this.btn_temizle.Text = "Temizle";
+            this.btn_temizle.UseVisualStyleBackColor = true;
+            this.btn_temizle.Click += new System.EventHandler(this.btn_temizle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_göster;
+        private System.Windows.Forms.ListBox Sınıfllar;
+        private System.Windows.Forms.TextBox txt_katnumarası;
+        private System.Windows.Forms.Label lbl_katnumarası;
+        private System.Windows.Forms.Label lbl_bilgi;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btn_temizle;
+    }
+}
+
---
+++ sınıf ödevi/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sınıf_ödevi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_göster_Click(object sender, EventArgs e)
+        {
+
+            if (txt_katnumarası.Text == "")
+            {
+
+                MessageBox.Show("Lütfen Bilgi Giriniz.");
+            }
+            else
+            {
+
+                int kat1 = 1;
+                int kat2 = 2;
+                int kat3 = 3;
+                int kat4 = 4;
+                int bilgi1 = int.Parse(txt_katnumarası.Text.ToString());
+
+                string[] sınıf9 = new string[5];
+                sınıf9[0] = "9/BT";
+                sınıf9[1] = "9/A";
+                sınıf9[2] = "9/B";
+                sınıf9[3] = "9/C";
+                sınıf9[4] = "9/M";
+                string[] sınıf10 = new string[5];
+                sınıf10[0] = "10/BT";
+                sınıf10[1] = "10/A";
+                sınıf10[2] = "10/B";
+                sınıf10[3] = "10/C";
+                sınıf10[4] = "10/M";
+                string[] sınıf11 = new string[5];
+                sınıf11[0] = "11/BT";
+                sınıf11[1] = "11/A";
+                sınıf11[2] = "11/B";
+                sınıf11[3] = "11/C";
+                sınıf11[4] = "11/M";
+                string[] sınıf12 = new string[5];
+                sınıf12[0] = "12/BT";
+                sınıf12[1] = "12/A";
+                sınıf12[2] = "12/B";
+                sınıf12[3] = "12/C";
+                sınıf12[4] = "12/M";
+
+                if (bilgi1 == 1)
+                {
+                    for (int a = 0; a < 5; a++)
+                    {
+                        Sınıfllar.Items.Add(sınıf9[a]);
+                    }
+
+                }
+                else if (bilgi1 == 2)
+                {
+                    for (int b = 0; b < 5; b++)
+                    {
+                        Sınıfllar.Items.Add(sınıf10[b]);
+                    }
+                }
+                else if (bilgi1 == 3)
+                {
+                    for (int c = 0; c < 5; c++)
+                    {
+                        Sınıfllar.Items.Add(sınıf11[c]);
+                    }
+                }
+                else if (bilgi1 == 4)
+                {
+                    for (int d = 0; d < 5; d++)
+                    {
+                        Sınıfllar.Items.Add(sınıf12[d]);
+                    }
+                }
+                if (bilgi1 > 4 || bilgi1 < 1)
+                {
+                    MessageBox.Show("Lütfen 1 İle 4 Arasında Bir Sayı Giriniz.");
+                }
+
+            }
+
+
+
+
+        }
+
+        private void btn_temizle_Click(object sender, EventArgs e)
+        {
+            Sınıfllar.Items.Clear();
+        }
+    }
+}
